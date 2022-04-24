1aa4b7ae06a8d7e1edbd33192e8ded130e66b817
Kevsely <elegis.sossou@yahoo.com>
2022-04-24 21:27:28 +0100

    Computedd the (date, hours, min, seconds) between the countdown date and today date

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ const inputContainer = document.getElementById("input-container")
 const countdownForm = document.getElementById("countdownForm")
 const dateEl = document.getElementById("date-picker")
 
-let countdownTitle = ''
+const countdownEl = document.getElementById("countdown")
+const countdownElTitle = document.getElementById("countdown-title")
+const countdonwBtn = document.getElementById("countdown-button")
+const timeElements = document.querySelectorAll("span")
+
+let countdownTitle = ""
 let countdownDate = ""
+let countdonwValue = Date
+
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
 
 //Set Date Input min with today's date
 const today  = new Date().toISOString().split("T")[0]
 dateEl.setAttribute("min", today)
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+    const now = new Date().getTime()
+    const distance = countdonwValue - now 
+
+    const days = Math.floor(distance / day)
+    const hours = Math.floor((distance % day) / hour)
+    const minutes = Math.floor((distance % hour) / minute)
+    const seconds = Math.floor((distance % minute) / second)
+}
 
 // Take values from form input
 function updateCountdown(e) {
@@ -15,6 +37,9 @@ function updateCountdown(e) {
     countdownTitle = e.srcElement[0].value
     countdownDate = e.srcElement[1].value
     console.log(countdownTitle, countdownDate)
+    // Get number version of current Date, update DOM
+    countdonwValue = new Date(countdownDate).getTime()
+    updateDOM()
 }
 
 // Event Listeners
